3cc1ced40e4474c1d498aba2d5c269fa169d7a35
Niklas Kasper <kasper.niklas@gmail.com>
2017-10-22 21:15:35 +0200

    Updated Application Main

--- src/main/java/com/userManagement/UserManagementApplication.java
+++ src/main/java/com/userManagement/UserManagementApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ItmbApplication {
+public class UserManagementApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ItmbApplication.class, args);
+		SpringApplication.run(UserManagementApplication.class, args);
 	}
 }
